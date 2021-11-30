dc791be199d8fc90673dbd5caffcac1f9290a668
Levente Ibrányi <ibranyilevente@gmail.com>
2021-11-30 16:58:56 +0100

    tudu.db add

--- tudu.sql
+++ tudu.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 30, 2021 at 12:26 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2021. Nov 30. 16:56
+-- Kiszolgáló verziója: 10.4.21-MariaDB
+-- PHP verzió: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tudu`
+-- Adatbázis: `tudu`
 --
 CREATE DATABASE IF NOT EXISTS `tudu` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `tudu`;
@@ -26,7 +26,7 @@ USE `tudu`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_data`
+-- Tábla szerkezet ehhez a táblához `user_data`
 --
 
 CREATE TABLE `user_data` (
@@ -40,16 +40,18 @@ CREATE TABLE `user_data` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_data`
+-- A tábla adatainak kiíratása `user_data`
 --
 
 INSERT INTO `user_data` (`user_id`, `user_first`, `user_last`, `user_register`, `username`, `user_pass`, `pass_reminder`) VALUES
-(1, 'Testfirstname', 'Testlastname', '2021-11-30', 'testusername', 'test1234', 'testreminder');
+(1, 'Testfirstname', 'Testlastname', '2021-11-30', 'testusername', 'test1234', 'testreminder'),
+(3, 'Post', 'Post', '2021-11-30', 'Postusername', 'Post1234', 'Postreminder'),
+(4, 'Post', 'Post', '2021-11-30', 'Postusername', 'Post1234', 'Postreminder');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_todo`
+-- Tábla szerkezet ehhez a táblához `user_todo`
 --
 
 CREATE TABLE `user_todo` (
@@ -59,16 +61,17 @@ CREATE TABLE `user_todo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_todo`
+-- A tábla adatainak kiíratása `user_todo`
 --
 
 INSERT INTO `user_todo` (`user_id`, `user_todo`, `todo_date`) VALUES
-(1, 'Letesztelni a programot.', '2021-11-30 12:21:42');
+(1, 'Letesztelni a programot.', '2021-11-30 12:21:42'),
+(2, 'Elso post', '2021-11-30 00:00:00');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_xp`
+-- Tábla szerkezet ehhez a táblához `user_xp`
 --
 
 CREATE TABLE `user_xp` (
@@ -77,55 +80,57 @@ CREATE TABLE `user_xp` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_xp`
+-- A tábla adatainak kiíratása `user_xp`
 --
 
 INSERT INTO `user_xp` (`user_id`, `user_exp`) VALUES
-(1, 223);
+(1, 223),
+(2, 345),
+(3, 111111);
 
 --
--- Indexes for dumped tables
+-- Indexek a kiírt táblákhoz
 --
 
 --
--- Indexes for table `user_data`
+-- A tábla indexei `user_data`
 --
 ALTER TABLE `user_data`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Indexes for table `user_todo`
+-- A tábla indexei `user_todo`
 --
 ALTER TABLE `user_todo`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Indexes for table `user_xp`
+-- A tábla indexei `user_xp`
 --
 ALTER TABLE `user_xp`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- A kiírt táblák AUTO_INCREMENT értéke
 --
 
 --
--- AUTO_INCREMENT for table `user_data`
+-- AUTO_INCREMENT a táblához `user_data`
 --
 ALTER TABLE `user_data`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `user_todo`
+-- AUTO_INCREMENT a táblához `user_todo`
 --
 ALTER TABLE `user_todo`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `user_xp`
+-- AUTO_INCREMENT a táblához `user_xp`
 --
 ALTER TABLE `user_xp`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
